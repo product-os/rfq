4ab371b759b6756a262876fca8064780c54780a1
rcooke-warwick <ryan@balena.io>
2020-10-15 17:18:16 +0100

    validate schema and check extension
    Change-type: patch
    Signed-off-by: Ryan Cooke <ryan@balena.io>

--- src/index.ts
+++ src/index.ts
@@ -12,14 +12,18 @@ const generate = async (
 	options: { output: string },
 ) => {
 	
-	const rfq = await validateSpec(Path.join(params.folder, 'source', 'specification', 'spec.json')); // generate rfq json from spec.json and schema
+	const specFile = JSON.parse(fs.readFileSync(Path.join(params.folder, 'source', 'specification', 'spec.json')).toString('utf8'));
+	const fileTypes = JSON.parse(fs.readFileSync(Path.join(__dirname,'../', 'hardware-types', specFile.hwType, 'fileTypes.json')).toString('utf8'));
+
+	const rfq = await validateSpec(specFile, specFile.hwType); // generate rfq json from spec.json and schema
 	zip.file('rfq.json', JSON.stringify(rfq, null, 2)); // add rfq.json to the release
+
 
 	const testFile = fs.readFileSync(Path.join(params.folder, 'testing', 'Testing.md'));
 	const testContent = Buffer.from(testFile).toString('utf8');
 	zip.file('Testing.md', testContent);
 
-	packageFiles(Path.join(params.folder, 'outputs'));
+	packageFiles(Path.join(params.folder, 'outputs'), fileTypes.fileTypes);
 
 	const data = await zip.generateAsync({
 		type: 'nodebuffer',
@@ -28,25 +32,27 @@ const generate = async (
 	fs.writeFileSync(Path.join(options.output, `release.zip`), data, 'binary');
 };
 
-// check the spec file against schema for the project type - at the moment this filters out fields not in the schema.
-const validateSpec = async(spec: string) => {
-	let specFile = JSON.parse(fs.readFileSync(spec).toString('utf8'));
-	const schema = await JSON.parse(fs.readFileSync(Path.join(__dirname,'../', 'hardware-types', specFile.hwType, 'rfq.json')).toString('utf8'));
-	const partRFQ = skhema.filter(schema, specFile);
+// check the spec file against schema for the project type
+const validateSpec = async(spec: string, hwType: string) => {
+	const schema = await JSON.parse(fs.readFileSync(Path.join(__dirname,'../', 'hardware-types', hwType, 'rfq.json')).toString('utf8'));
+	skhema.validate(schema, spec); // if spec.json not valid, or missing parameters, error gets thrown here
+	const partRFQ = skhema.filter(schema, spec);
 	return partRFQ;
 };
 
 
 // get all files in output folder - should we restrict the file types to get depending on project type
-const packageFiles = async(path: string) => {
+const packageFiles = async(path: string, fileTypes:Array<String>) => {
 	fs.readdirSync(path, {withFileTypes: true}).forEach(fileName => { // nead this to be recursive 
 		// if its a directory, do it again - if its a file, zip it 
 		if(fileName.isDirectory()){
-			packageFiles(Path.join(path, fileName.name));
+			packageFiles(Path.join(path, fileName.name), fileTypes);
 		} else {
-			const fileRead = fs.readFileSync(Path.join(path, fileName.name))
-			const fileContent = Buffer.from(fileRead).toString('utf8');
-			zip.file(fileName.name, fileContent) // what should the files get called? is it ok just to dump all the files in there?
+			if(fileTypes.includes(Path.extname(fileName.name))){ // if file extension is in filetypes.json then add to the zip
+				const fileRead = fs.readFileSync(Path.join(path, fileName.name))
+				const fileContent = Buffer.from(fileRead).toString('utf8');
+				zip.file(fileName.name, fileContent) // what should the files get called? is it ok just to dump all the files in there?
+			}
 		}
 	});
 };
